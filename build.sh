4bc53db4de0b219ca15a6fc176023490b32d7b11
Clemens Bergmann <clemens.bergmann@tu-darmstadt.de>
2021-04-10 08:13:05 +0200

    show edit link on private repo

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 cd ~/doku
 git pull
 git reset --hard
-DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr NODE_PATH="$(/usr/bin/npm -g root)" ~/bin/antora --generator antora-site-generator-lunr --fetch --clean antora-playbook.yml
+FORCE_SHOW_EDIT_PAGE_LINK=true DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr NODE_PATH="$(/usr/bin/npm -g root)" ~/bin/antora --generator antora-site-generator-lunr --fetch --clean antora-playbook.yml
