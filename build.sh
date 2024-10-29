09ee9d2bb295a8efa314fc6d906257f640ab719a
Clemens Bergmann <clemens.bergmann@tu-darmstadt.de>
2024-10-29 23:46:08 +0100

    new path for antora

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 cd ~/doku
 git pull
 git reset --hard
-FORCE_SHOW_EDIT_PAGE_LINK=true NODE_PATH="$(/usr/bin/npm -g root)" ~/bin/antora --fetch --clean antora-playbook.yml
+FORCE_SHOW_EDIT_PAGE_LINK=true NODE_PATH="$(/usr/bin/npm -g root)" node_modules/.bin/antora --fetch --clean antora-playbook.yml
